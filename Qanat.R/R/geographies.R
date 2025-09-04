c127267071f7dfdd3e5d06df72c8ee53d9ecf792
Travis Hinkelman <THinkelman@esassoc.com>
2025-09-03 17:47:04 -0700

    Update geographies() with new approach

--- Qanat.R/R/geographies.R
+++ Qanat.R/R/geographies.R
@@ -6,11 +6,9 @@
 #' @md
 #' @param simplify      Coerce to vector, data frame, or matrix
 #' @param user_key      Qanat user key
-#' @param use_qa        Boolean flag to use QA version of Qanat
 #' @export
 
-geographies <- function(simplify = TRUE, user_key = get_user_key(), use_qa = TRUE) {
-  glue::glue("{api_url(use_qa)}/geographies") |>
-    get_qanat(user_key) |>
-    httr2::resp_body_json(simplifyVector = simplify)
+geographies <- function(simplify = TRUE, user_key = get_user_key()) {
+  glue::glue("{api_url()}/geographies") |>
+    get_qanat(simplify, user_key)
 }
